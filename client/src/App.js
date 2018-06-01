6c94ce6d2937b810965e0eb30d198d3c6bb88d47
chashikajw <chashikajw007@gmail.com>
2018-06-01 16:44:37 +0530

    add proxy

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+    state = {
+        response: ''
+    };
+
+    componentDidMount() {
+        this.callApi()
+            .then(res => this.setState({ response: res.express }))
+            .catch(err => console.log(err));
+    }
+
+    callApi = async () => {
+        const response = await fetch('/api/hello');
+        const body = await response.json();
+
+        if (response.status !== 200) throw Error(body.message);
+
+        return body;
+    };
+
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+            To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+        </div>
     );
-  }
+    }
 }
 
 export default App;
