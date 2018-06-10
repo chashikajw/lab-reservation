2bf48b11770eab6f1279dfdf9a3d4b797e0ba69c
chashikajw <chashikajw007@gmail.com>
2018-06-11 01:53:06 +0530

    add bookingdays reducer

---
+++ client/src/reducers/reducer-bookdays.js
@@ -0,0 +1,67 @@
+
+export default function () {
+
+    return [
+        {
+            id: 1,
+            hall: "LabA",
+            date: "2018/06/11",
+            book_times: ["10-12" , "1-3"]
+
+        },
+        {
+            id: 2,
+            hall: "W002",
+            date: "2018/06/11",
+            book_times: ["10-12" , "1-3"]
+        },
+        {
+            id: 3,
+            hall: "4th floor",
+            date: "2018/06/11",
+            book_times: ["10-12" , "1-3"]
+        },
+        {
+            id: 4,
+            hall: "w002",
+            date: "2018/06/11",
+            book_times: ["10-12" , "1-3"]
+        },
+        {
+            id: 5,
+            hall: "w001",
+            date: "2018/06/11",
+            book_times: ["10-12" , "1-3"]
+        },
+        {
+            id: 6,
+            hall: "w001",
+            date: "2018/06/11",
+            book_times: ["10-12" , "1-3"]
+        },
+        {
+            id: 7,
+            hall: "w002",
+            date: "2018/06/11",
+            book_times: ["10-12" , "1-3"]
+        },
+        {
+            id: 8,
+            hall: "w002",
+            date: "2018/06/11",
+            book_times: ["10-12" , "1-3"]
+        },
+        {
+            id: 9,
+            hall: "w002",
+            date: "2018/06/11",
+            book_times: ["10-12" , "1-3"]
+        },
+        {
+            id: 10,
+            hall: "w002",
+            date: "2018/06/11",
+            book_times: ["10-12" , "1-3"]
+        }
+    ]
+}
