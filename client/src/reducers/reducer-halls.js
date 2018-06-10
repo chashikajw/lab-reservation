8e9712d15f6fb4ff1c6656e47152aeaab475707b
chashikajw <chashikajw007@gmail.com>
2018-06-11 01:57:47 +0530

    add hall reducer

---
+++ client/src/reducers/reducer-halls.js
@@ -0,0 +1,57 @@
+
+export default function () {
+
+    return [
+        {
+            id: 1,
+            name: "W001",
+            book_days: []
+
+        },
+        {
+            id: 2,
+            name: "W002",
+            book_days: []
+        },
+        {
+            id: 3,
+            name: "4th floor",
+            book_days: []
+        },
+        {
+            id: 4,
+            name: "Mini audi",
+            book_days: []
+        },
+        {
+            id: 5,
+            name: "W002",
+            book_days: []
+        },
+        {
+            id: 6,
+            name: "Irque lab",
+            book_days: []
+        },
+        {
+            id: 7,
+            name: "LabA",
+            book_days: []
+        },
+        {
+            id: 8,
+            name: "LabB",
+            book_days: []
+        },
+        {
+            id: 9,
+            name: "LabC",
+            book_days: []
+        },
+        {
+            id: 10,
+            name: "LabD",
+            book_days: []
+        }
+    ]
+}
