bc3b441e02798e1cccb3c4ac163dede14feed265
chashikajw <chashikajw007@gmail.com>
2018-06-22 13:57:49 +0530

    add reservation test data

---
+++ seeds/reservations.js
@@ -0,0 +1,41 @@
+var reservation = require('../models/reservation');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/lab_reservation');
+
+var reservations = [
+    new reservation({
+            hallname: "W002" ,
+            date_time: "2018/06/25 10-12",
+            reserve_person: "chashika weerathung",
+            reason: "book hall 2",
+            permission_by: "Chamath sir",
+            is_accepted: true
+
+        }
+    ),
+    new reservation({
+            hallname: "W002" ,
+            date_time: "2018/06/25 10-12",
+            reserve_person: "chashika weerathung",
+            reason: "book hall 2",
+            permission_by: "Chamath sir",
+            is_accepted: true
+
+        }
+    ),
+    new reservation({
+            hallname: "W002" ,
+            date_time: "2018/06/25 10-12",
+            reserve_person: "chashika weerathung",
+            reason: "book hall 2",
+            permission_by: "Chamath sir",
+            is_accepted: true
+
+        }
+    )
+];
+
+for(var i=0; 3 > i; i++){
+    reservations[i].save();
+}
+
