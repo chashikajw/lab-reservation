3aa5489f03d90b575f1036c5fe5087996f10cf76
chashikajw <chashikajw007@gmail.com>
2018-06-27 02:49:20 +0530

    modify reservation model

--- models/reservation.js
+++ models/reservation.js
@@ -2,13 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
+    email: {type:String, required:true},
     hallname: {type:String, required:true},
-    date_time: {type:String, required:true},
+    Date: {type:String, required:true},
+    timeto: {type:String, required:true},
+    timefrom: {type:String, required:true},
     reserve_person: {type:String, required:true},
-    reseaon:{type:String, required:true},
-    permission_by: {type:String, required:true}
+    reason:{type:String, required:true},
+    permissonedby: {type:String, required:true},
     is_accepted:{type:Boolean}
 
 });
+
 
 module.exports = mongoose.model('Reservation', schema);
