af99a58b22c35cc8cfb9d8a6f724b3bafcadd860
chashikajw <chashikajw007@gmail.com>
2018-06-27 02:48:25 +0530

    add backen request

--- app.js
+++ app.js
@@ -7,14 +7,22 @@ var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
+var bodyParser = require('body-parser');
+var reservationData = require('./models/reservation');
 
 var indexRouter = require('./routes/index');
+var reservations = require('./routes/reservations');
+var Validator = require('validator');
+
+
 
 
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017/lab_reservation');
 var db = mongoose.connection;
+
+app.use(bodyParser.json());
 
 //handle mongo error
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -51,6 +59,173 @@ app.get('/api/bookings', (req, res) => {
 });
 
 
+
+app.post('/api/searchBookings', (req,res,next) => {
+
+    var date = req.body.reservation.Date.toString();
+    var hall = req.body.reservation.hallname;
+    console.log(date);
+    console.log(hall);
+    reservationData.find(
+        {
+            $and :
+            [
+                {"hallname": hall }
+
+
+            ]
+        },
+        function (err, docs) {
+            res.json(docs);
+        }
+    );
+
+});
+
+app.post('/api/getreservations', function(req, res, next) {
+    reservationData.find(function (err,docs) {
+        //res.render({ title: 'Express', reservations: docs, layout: 'layout.hbs' });
+        res.json();
+    });
+
+});
+
+function isEmpty(arr) {
+    if(arr.length== 0){
+        return true;
+    }else{
+        return false;
+    }
+
+}
+
+function validateInput(frm,to,hall,date){
+
+    reservationData.find(
+       /* {
+            $and: [
+                {
+                    "hallname" : hall
+                },
+                {
+                    "Date" : date
+                },
+                {
+                    $or : [
+                        {
+                            "timefrom" : {$gt: frm, $gt: to}
+                            },
+                        {
+                            "timeto" : {$lt: frm, $lt: to}
+                            },
+                        {
+                            $and:
+                            [
+                                {
+                                    "timefrom" : {$lt: frm, $gt: to}
+                                },
+                                {
+                                    "timeto" : {$lt: frm, $lt: to}
+                                },
+                            ]
+                        },
+                        {
+                            $and:
+                            [
+                                {
+                                    "timefrom" : {$gt: frm, $gt: to}
+                                },
+                                {
+                                    "timeto" : {$lt: frm, $gt: to}
+                                },
+                            ]
+                        },
+                        {
+                            $and:
+                            [
+                                {
+                                    "timefrom" : {$gt: frm, $gt: to}
+                                },
+                                {
+                                    "timeto" : {$lt: frm, $lt: to}
+                                },
+                            ]
+                        },
+                    ]
+                },
+
+            ]
+        },*/
+        {"hallname" : hall},
+        {"Date" : date},
+        function (err,res) {
+            let errors = {};
+            if(err){
+                console.log(err);
+            }else{
+                console.log(res);
+
+                if(res.length > 0){
+
+                    errors.timefrom = "this time is already booked";
+                    return {
+                        errors,
+                        isValid: false
+                    }
+
+                    res.json(errors);
+                }else{
+                    //res.json({success: true});
+                    return {
+                        errors,
+                        isValid: true
+                    }
+                    res.json(errors);
+                }
+            }
+        }
+    );
+
+
+
+}
+
+app.post('/api/reservations', (req,res,next) =>{
+        var frm = req.body.reservation.timefrom;
+        var to = req.body.reservation.timeto;
+        var date = req.body.reservation.Date;
+        var hall = req.body.reservation.hallname;
+       /* const { errors, isValid} = validateInput(frm,to,hall,date);
+        if (!isValid){
+
+            res.json(errors);
+        }*/
+
+
+    var Reservation  =  {
+            email: req.body.reservation.email,
+            hallname: req.body.reservation.hallname,
+            Date: req.body.reservation.Date.toString(),
+            timeto: req.body.reservation.timeto,
+            timefrom: req.body.reservation.timefrom,
+            reserve_person: req.body.reservation.reserve_person,
+            reason: req.body.reservation.reason,
+            permissonedby: req.body.reservation.permissonedby,
+            is_accepted: req.body.reservation.is_accepted
+
+        }
+
+
+        reservationData.create(Reservation, function (error, reservation) {
+            if (error) {
+                //console.log(error);
+                return next(error);
+            } else {
+                console.log("succefully inserted");
+            }
+        });
+    //console.log(req.body.reservation.email);
+});
 
 
 // view engine setup
